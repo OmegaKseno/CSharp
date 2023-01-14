fd11cba136ad64f08e700e2ac4c82ce9e84fda30
Maksim.K <omega.kseno@gmail.com>
2023-01-15 02:52:44 +0300

    Add Task008

---
+++ Task08/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+Console.Write("Insert the number ->");
+int number = Convert.ToInt32(Console.ReadLine());
+int counter = 1;
+while(counter <= number)
+{
+     if(counter%2==0)
+       {
+          Console.Write(counter+"|");
+       }
+     counter++;
+}
