3a7d1111cb6e8c40b049d43b95baba007d371a32
Maksim.K <omega.kseno@gmail.com>
2023-01-15 01:37:20 +0300

    Add Task005

---
+++ Task05/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая на вход принимает
+//одно число (N), а на выходе показывает все целые
+//числа в промежутке от -N до N.
+
+Console.Write("Insert the number-> ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number>0)
+{
+int negative = -number;
+while(negative <= number)
+{
+    Console.Write(negative +",");
+    negative++;
+}
+}
+else
+{
+    Console.WriteLine("Entered non-natural number");
+}
+
+
