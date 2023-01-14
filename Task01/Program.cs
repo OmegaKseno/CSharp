61a800e31d4ba1d97df7360f7ad40d0be231844e
Maksim.K <omega.kseno@gmail.com>
2023-01-14 22:29:21 +0300

    Add Task001

---
+++ Task01/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает два
+//числа и проверяет, является ли первое число квадратом
+//второго.
+Console.Write("Enter the first number->");
+double FirstNumber = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter the second number->");
+double SecondNumber = Convert.ToDouble(Console.ReadLine());
+if(SecondNumber*SecondNumber==FirstNumber)
+{
+    Console.WriteLine("Yes! The number is the square of the second");
+}
+else
+{
+    Console.WriteLine("No! The number is not the square of the second");
+}
