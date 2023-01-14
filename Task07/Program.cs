a6fdcd29877a6f6986120457c7b488a42509d15c
Maksim.K <omega.kseno@gmail.com>
2023-01-15 01:49:50 +0300

    Add Task007

---
+++ Task07/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая на вход принемает
+//трехзначное число и на выходе показывает последнюю цифру.
+Console.Write("Insert the number ->");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number<1000&number>99)
+{
+    Console.WriteLine(number%10);
+}
+else
+{
+    Console.WriteLine("Incorrect number entered");
+}
