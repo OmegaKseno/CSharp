006e1b4c05b0643a3303a451ba4258f9b860dc80
Maksim.K <omega.kseno@gmail.com>
2023-01-15 02:30:58 +0300

    Add Task006

---
+++ Task06/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт, 
+//является ли число чётным (делится ли оно на два без остатка).
+Console.Write("Insert the number -> ");
+double Number = Convert.ToDouble(Console.ReadLine());
+if(Number%2==0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+   Console.WriteLine("Нет");  
+}
