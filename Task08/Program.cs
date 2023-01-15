c6f0e58ca9bc76e4c0c3e3dfb7695f176c93a429
Maksim.K <omega.kseno@gmail.com>
2023-01-15 03:09:56 +0300

    Правка Task08

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -2,7 +2,7 @@
 // а на выходе показывает все чётные числа от 1 до N.
 Console.Write("Insert the number ->");
 int number = Convert.ToInt32(Console.ReadLine());
-int counter = 1;
+int counter = 2;
 while(counter <= number)
 {
      if(counter%2==0)
