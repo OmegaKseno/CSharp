dcd353ec01226ad157a117860cf939770623485a
Maksim.K <omega.kseno@gmail.com>
2023-01-19 02:04:37 +0300

    add Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного 
+//числа или сообщает, что третьей цифры нет. Выполнить с помощью числовых операций 
+//(целочисленное деление, остаток от деления).
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+Console.Write("Введите число -> ");
+int number = Convert.ToInt32(Console.ReadLine());
+int temp;
+if (number>100)
+{
+    temp = DigitCorrection(number);
+    Console.WriteLine(number +" -> "+ temp);
+}   
+else 
+{
+    Console.WriteLine (number+" <- Отсутствует третья цифра");
+}
+
+int DigitCorrection(int dNum)
+{
+while(dNum>999)
+{
+    dNum = dNum/10;
+}
+return dNum%10;
+}
+ 
+
+ 
