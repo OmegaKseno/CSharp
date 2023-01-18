7ad201fa8484935dd79fb68b7bf0948982d0c2cb
Maksim.K <omega.kseno@gmail.com>
2023-01-19 01:03:32 +0300

    add Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает
+ //вторую цифру этого числа. Выполнить с помощью числовых операций 
+ //(целочисленное деление, остаток от деления).
+//456 -> 5
+//782 -> 8
+//918 -> 1
+Console.Write("Введте трёхзначное число -> ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number>100&&number<999)
+{
+int result = SecondDigit(number);
+Console.WriteLine(number+ "  -> " +result);
+}
+else
+{
+    Console.WriteLine("Введено не корректное число");
+}
+
+
+int SecondDigit (int num)
+{
+ return (num/10)%10;   
+}
