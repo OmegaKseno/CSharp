e12d7e052c91f1873a1b98a5d45f867c6a87cff1
Maksim.K <omega.kseno@gmail.com>
2023-01-18 20:30:01 +0300

    add Task12

---
+++ Task12/Program.cs
@@ -0,0 +1,32 @@
+﻿//12. Напишите программу, которая будет принимать на
+//вход два числа и выводить, является ли первое число
+//кратным второму. Если число 1 не кратно числу 2, то
+//программа выводит остаток от деления.
+//34, 5 -> не кратно, остаток 4
+//16, 4 -> кратно
+int MultiDig (int num1,int num2)
+{
+int res = num1%num2;
+return res;
+}
+
+Console.Write("Введите чило 1 ->  ");
+int firstDigit = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите чило 2 ->  ");
+int secondDigit = Convert.ToInt32(Console.ReadLine());
+int result = MultiDig(firstDigit,secondDigit);
+if (result==0)
+{
+ Console.WriteLine(firstDigit+" , "+secondDigit+", кратно.");   
+}
+else
+{
+Console.WriteLine(firstDigit+" , "+secondDigit+",не кратно, остаток "+result);
+}
+
+
+
+
+
+
+
