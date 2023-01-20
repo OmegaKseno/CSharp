459df408c7bfa2c46415222b22d54543d087c5b6
Maksim.K <omega.kseno@gmail.com>
2023-01-21 00:03:32 +0300

    Add Task22

---
+++ Task22/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу которая на вход принемает число (N)
+//и выдает таблицу квадратов чисел от 1 до N.
+//5-> 1,4,9,16,25
+Console.Write("Введите число (N) ->");
+int digital = Convert.ToInt32(Console.ReadLine());
+PrintSquares(digital);
+void PrintSquares(int number)
+{
+int temp = 1;
+while(temp<=number)
+{
+    Console.WriteLine($"| {temp}|{temp*temp}   ");
+    temp++;
+    Console.WriteLine("|__|___|");
+}
+}
