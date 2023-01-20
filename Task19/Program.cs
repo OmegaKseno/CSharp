ace46c7e8fef3ed6ad766bfeac334de217efce05
Maksim.K <omega.kseno@gmail.com>
2023-01-21 01:30:25 +0300

    Add Task19

---
+++ Task19/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задача 19
+//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+bool CheckPalindrome(int num)
+{
+    if( num%10==num/10000)
+    {
+        return (num/10)%10==(num/1000)%10;    
+    }
+    else
+    {
+        return false;
+    }
+}
+Console.Write("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number > 9999&number<100000)
+{
+     string status = CheckPalindrome(number)==true?("Да"):("Нет");
+     Console.WriteLine(number+" -> "+status);
+
+}
+else
+{
+     Console.WriteLine("Введено не корректное число");
+}
+
