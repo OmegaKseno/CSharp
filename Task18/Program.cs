a1977c69c8053be8f078480ec1cdeb36251a1acd
Maksim.K <omega.kseno@gmail.com>
2023-01-22 00:15:04 +0300

    Add Task18

---
+++ Task18/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 18: Напишите программу, которая по
+//заданному номеру четверти, показывает диапазон
+//возможных координат точек в этой четверти (x и y).
+Console.Write("Введите номер четверти: ");
+int quarter = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(CheckQuarter(quarter));
+
+ string CheckQuarter (int metQ )
+{
+ if (metQ==1)return "x>0,y>0";
+ if (metQ==2)return "x>0,y<0";
+ if (metQ==3)return "x<0,y<0";
+ if (metQ==4)return "x<0,y>0";
+
+ return "Введите корректные данные";
+}
