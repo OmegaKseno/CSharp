17f5fac53cf08ab497d40c4c80708f8f066f3592
Maksim.K <omega.kseno@gmail.com>
2023-01-24 22:59:52 +0300

    Add Task30

--- Task00/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿//Написать программу, которая на вход
-//принимает число и выдает его квадрат
-//(число умножкнное на само себя).
-// Пример  4->16
-Console.Write("Insert the number: ");
-int number = Convert.ToInt32 (Console.ReadLine());
-Console.WriteLine($"square number  { number } = {number*number}");
