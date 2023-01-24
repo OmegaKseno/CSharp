2f7a04f032744bc0aefc79417de6fd7d1eaa6690
Maksim.K <omega.kseno@gmail.com>
2023-01-25 00:46:41 +0300

    correction Task25

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,25 +1,30 @@
-﻿Console.Write("Введите первое число А-> ");
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа
+// (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+int NaturalDegree(int numA, int numB)
+{
+
+    int temp = numA;
+    for (int i = 0; i < numB - 1; i++)
+    {
+        temp = numA * temp;
+    }
+    return temp;
+}
+
+Console.Write("Введите первое число А-> ");
 int numberA = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите первое число B-> ");
 int numberB = Convert.ToInt32(Console.ReadLine());
 
-
-if (numberA>0&&numberB>0)
+if (numberA > 0 && numberB > 0)
 {
-Console.Write(NaturalDegree(numberA,numberB));
+    int result = NaturalDegree(numberA, numberB);
+    Console.Write(numberA+", "+numberB+" -> "+result);
 }
 else
 {
     Console.Write("Введено не корректное значение");
 }
 
-int NaturalDegree (int numA,int numB)
-{
-    
-    int temp = numA;
-    for (int i = 0; i < numB-1; i++)
-    {
-       temp =  numA*temp;
-    }
-    return temp;
-}
