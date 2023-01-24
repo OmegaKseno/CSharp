c40af810a6282b8f1db795188842563363701b0a
Maksim.K <omega.kseno@gmail.com>
2023-01-24 23:57:11 +0300

    Add Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Введите первое число А-> ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите первое число B-> ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+
+if (numberA>0&&numberB>0)
+{
+Console.Write(NaturalDegree(numberA,numberB));
+}
+else
+{
+    Console.Write("Введено не корректное значение");
+}
+
+int NaturalDegree (int numA,int numB)
+{
+    
+    int temp = numA;
+    for (int i = 0; i < numB-1; i++)
+    {
+       temp =  numA*temp;
+    }
+    return temp;
+}
