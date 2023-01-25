b5c2bd49b699d4a0c3cac657e90e070ab790ce16
Maksim.K <omega.kseno@gmail.com>
2023-01-25 20:59:47 +0300

    Add Task24

---
+++ Task24/Program.cs
@@ -0,0 +1,16 @@
+﻿//24.
+//Напишите программу которая, принимает на вход число А и выдает
+//сумму чисел от 1 до а.
+Console.Write("Введите число А -> ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result=Summer(number);
+Console.Write(number+"-> "+result);
+int Summer(int num)
+{
+int sum=0;
+for (int i = 1; i <= num; i++)
+{
+    sum = sum+i;
+}
+return sum;
+}
