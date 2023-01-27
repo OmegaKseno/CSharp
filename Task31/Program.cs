f94a0e4cb3b730d1f2328423e9f179fc945774c0
Maksim.K <omega.kseno@gmail.com>
2023-01-27 23:34:34 +0300

    Add Task31

---
+++ Task31/Program.cs
@@ -0,0 +1,73 @@
+﻿//Задача 31: Задайте массив из 12 элементов, заполненный
+//случайными числами из промежутка [-9, 9]. Найдите сумму
+//отрицательных и положительных элементов массива.
+//Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма
+//положительных чисел равна 29, сумма отрицательных равна
+//-20.
+void PrintArray(int[] printArray)
+{
+    Console.Write("[");
+    for (int i = 0; i < printArray.Length; i++)
+    {
+
+        if (i == printArray.Length - 1)
+        {
+            Console.Write(printArray[i]);
+        }
+        else
+        {
+            Console.Write($"{printArray[i]} , ");
+        }
+    }
+    Console.Write("]");
+    Console.WriteLine(" ");
+}
+
+int[] CreationArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max);
+    }
+    return arr;
+}
+int SumOfNegativeNumbers(int[] arr)
+
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] < 0)
+        {
+            sum += arr[i];
+        }
+
+    }
+    return sum;
+}
+
+int SumOfPositiveNumbers(int[] arr)
+
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+        {
+            sum += arr[i];
+        }
+
+    }
+    return sum;
+}
+
+
+
+int[] array = CreationArray(12, -9, 10);
+PrintArray(array);
+int sumNegative = SumOfNegativeNumbers(array);
+int sumPositive = SumOfPositiveNumbers(array);
+Console.WriteLine($"Сумма отриуательных чисел равна : {sumNegative}");
+Console.WriteLine($"Сумма положительных чисел равна:   {sumPositive}");
