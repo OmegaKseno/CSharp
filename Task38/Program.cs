1ab435687574e8155b98673fe746ba4f66e76259
Maksim.K <omega.kseno@gmail.com>
2023-01-28 23:39:20 +0300

    Add Task38

---
+++ Task38/Program.cs
@@ -0,0 +1,53 @@
+﻿//Задача 38: Задайте массив вещественных чисел.
+//Найдите разницу между максимальным и минимальным
+//элементами массива.
+//[3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
+double[] CreationArrayDouble(int size, int min, int max)
+{
+    double[] arr = new double[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.NextDouble() * (max - min) + min;
+    }
+    return arr;
+}
+
+void PrintArrayDouble(double[] printArray)
+{
+    Console.Write("[");
+    for (int i = 0; i < printArray.Length; i++)
+    {
+        if (i == printArray.Length - 1)
+        {
+
+            Console.Write(Math.Round(printArray[i], 1));
+        }
+        else
+        {
+
+            Console.Write($"{Math.Round(printArray[i], 1)} | ");
+        }
+    }
+    Console.Write("]");
+
+}
+
+double DifferenceMinMax(double[] arr)
+{
+    double min = arr[0];
+    double max = arr[0];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] < min) min = arr[i];
+
+        if (arr[i] > max) max = arr[i];
+    }
+
+    return max - min;
+}
+
+double[] array = CreationArrayDouble(6, 1, 100);
+PrintArrayDouble(array);
+double result = DifferenceMinMax(array);
+Console.Write($" -> {Math.Round(result, 1)}");
