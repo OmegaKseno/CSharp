081afe345c7ce002a5c98d3069d23a14f366a04f
Maksim.K <omega.kseno@gmail.com>
2023-02-02 22:50:12 +0300

    Add Task 42

---
+++ Task42/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 42: Напишите программу, которая будет преобразовывать
+//десятичное число в двоичное.
+//45 -> 101101
+//3 -> 11
+//2 -> 10
+Console.Write("Введите число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int IntToBool(int num)
+{
+    int res = 0;
+    int temp = 1;
+    while(num>0)
+    {
+      res = res + (num%2*temp);
+      num = num/2;
+      temp*=10;
+
+    }
+    return res;
+}
+int result = IntToBool(number);
+Console.Write(result);
