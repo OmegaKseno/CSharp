a286d05980cbb20aa69e43f53c2ba701f753b095
Maksim.K <omega.kseno@gmail.com>
2023-02-08 00:13:51 +0300

    add 55

---
+++ Task55/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 55: Задайте двумерный массив. Напишите программу,
+// которая заменяет строки на столбцы. В случае, если это
+// невозможно, программа должна вывести сообщение для
+// пользователя.
+
+
+int[,] Creation2dArrayIntNumber(int line, int column, int min, int max)
+{
+    int[,] matrix = new int[line, column];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+void Print2dArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4} | ");
+            else Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+
+int[,]array2D = Creation2dArrayIntNumber(5, 5, 1, 10);
+Print2dArray(array2D);
+ReplacingRowsWithColumns (array2D);
+Console.WriteLine(" ");
+Print2dArray(array2D);
+
+
+void ReplacingRowsWithColumns (int[,]matrix)
+{
+int tempI = 0;
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+   for  (int j = i+1; j <matrix.GetLength(1); j++)
+    {
+        tempI = matrix[i,j];
+        matrix[i,j]=matrix[j,i];
+        matrix[j,i]=tempI;
+    }
+}
+
+
+}
