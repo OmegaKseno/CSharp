62de339e5b84cd4926a2fed13800afb5ea3a1ebb
Maksim.K <omega.kseno@gmail.com>
2023-02-12 00:45:48 +0300

    Add Task67

---
+++ Task67/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 67: Напишите программу, которая будет принимать на вход число и
+//возвращать сумму его цифр.
+//453 -> 12
+//45 -> 9
+
+Console.Write("введите целое число ->");
+int number = Convert.ToInt32(Console.ReadLine());
+int NumberSum(int num)
+{
+    if (num != 0)
+    {
+        return num % 10 + NumberSum(num / 10);
+    }
+    return num;
+}
+
+Console.WriteLine(NumberSum(number));
+
